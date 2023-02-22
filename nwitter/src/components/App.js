969ed36ea0e09d5fd3182c0b49c39e1319ab0b60
yeongjin Ha <eclips1024@naver.com>
2023-02-22 18:59:11 +0900

    error 해결 및 google & github Login 연동

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouters from "components/Router";
 import {authService} from "fbase"
 
 //모든 로직을 다룰 공간
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); // useState의 기본값에 firebase.auth()를 사용해 유저가 로그인을 했는지, 안했는지의 여부를 체크하려고 함.
+  const [init, setInit] = useState(false)
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // useState의 기본값에 firebase.auth()를 사용해 유저가 로그인을 했는지, 안했는지의 여부를 체크하려고 함.
+    useEffect(()=>{
+      authService.onAuthStateChanged((user)=>{
+        if(user){
+          setIsLoggedIn(true);
+        }else{
+          setIsLoggedIn(false)
+        }
+          setInit(true)
+        });
+      }, []);
   return (
     <>
-    <AppRouters isLoggedIn={isLoggedIn}/>
+    {init ? <AppRouters isLoggedIn={isLoggedIn}/>: "Initialize"}
     <footer>&copy; Nwitter {new Date().getFullYear()}Nwitter</footer>
     </>
   );
--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,64 +1,111 @@
-import {
-    getAuth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    } from 'firebase/auth';
+import app, { authService, firebaseInstance } from "fbase";
 import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
-const Auth = () =>{ 
+const Auth = ({}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
 
-    // 동적 form을 만들기 위한 react Hook UseState 활용
-    const [email, setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [newAccount,setNewAccount] = useState(true);
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    let data;
+    if (newAccount) {
+      createUserWithEmailAndPassword(authService, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
 
-    // 이벤트 동작 함수 만들기
-    const onChange = (event) => {
-        const {target:{name,value},
-        } = event;
-        if(name === 'email') {
-            setEmail(value);
-        } else if (name === 'password'){
-            setPassword(value);
-        }
-    };
-    const onSubmit = async(event) => {
-        event.preventDefault();
-        try{
-            let data;
-            const auth = getAuth();
-            if(newAccount) {
-                //Create account
-                data = await createUserWithEmailAndPassword(auth,email, password);
-            }else{
-                // login
-                data = await signInWithEmailAndPassword(auth,email, password);
-            }
-            console.log(data)
-        } catch(err) {
-            console.log(err)
-        }
-    };
-return( 
-<div>
-    <form onSubmit={onSubmit}>
-        <input name="email"
-        type="email" placeholder="Email"
-        required 
-        defalutvalue={email} onChange={onChange} />
-
-      <input name="Password"
-      type="password" placeholder="Password" 
-      required 
-      defalutvalue={password} onChange={onChange} />
-
-        <input type="submit" defalutvalue={newAccount ? "Create Account" : "LogIn"}/>
-    </form>
+          // ...
+        })
+        .catch((error) => {
+          const errorMessage = error.message;
+          setError(errorMessage);
+          setError(errorMessage);
+          // ..
+        });
+    } else {
+      signInWithEmailAndPassword(authService, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorMessage);
+          setError(errorMessage);
+        });
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await signInWithPopup(authService, provider);
+    console.log(data);
+  };
+  return (
     <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
+      <div>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
+      </div>
     </div>
-</div>
-)
-}
+  );
+};
 export default Auth;
