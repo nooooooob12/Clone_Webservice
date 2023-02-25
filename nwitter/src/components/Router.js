001866b26b10f9bbc92e30abc5b122999ba0db62
yeongjin Ha <eclips1024@naver.com>
2023-02-25 21:43:00 +0900

    react-router-dom v6 버전 코드 작성

--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -1,25 +1,23 @@
 import {React} from 'react';
-import {HashRouter as Router, Route, Routes,} from 'react-router-dom';
-import Auth from 'routes/Auth';
+import {HashRouter as Route, Routes,} from 'react-router-dom';
+
 import Home from 'routes/Home';
 import Profile from 'routes/Profile';
 import Navigation from './Navigation';
 
 const AppRouters =({isLoggedIn})=> {
     return(
-        <Router>
+        <>
             {isLoggedIn && <Navigation/>}
          <Routes>
             {isLoggedIn ? (
-            <Route exact path="/">
-                <Home/>
+            <Route exact path="/" element={ <Home/>}>
             </Route> ) :(
-                 <Route exact path="/profile">
-                    <Profile/>
+                 <Route exact path="/profile" element={<Profile/>}>
                 </Route>
             )}
          </Routes>
-        </Router>
+         </>
     )
 }
 export default AppRouters;
